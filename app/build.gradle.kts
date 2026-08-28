0d4cc68d655339c1a09511afaaa0277aef51c4e2
ScoobyDouche <43378931+ScoobyDouche@users.noreply.github.com>
2026-08-27 20:17:04 -0400

    Bump to 1.8.2 live load test monitor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.phonediagnostic"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.8.1"
+        versionCode = 16
+        versionName = "1.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
